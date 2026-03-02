da58a766ef3337e9a8a863cb0d24eea7b948a01b
Md Sufiyan Azmat <71426452+MdSufiyanAzmat@users.noreply.github.com>
2026-03-02 22:18:53 +0530

    Time: 7 ms (69.81%), Space: 15.4 MB (13.92%) - LeetHub

---
+++ 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numSplits(string s) {
+        int n = s.size();
+       vector<int> pref(n);
+       vector<int> suffix(n);
+       unordered_map<char, int> freq;
+       for(int i=0; i<n; i++){
+        freq[s[i]]++;
+        pref[i] = freq.size();
+       } 
+       freq.clear();
+
+       for(int i=n-1; i>=0; i--){
+        freq[s[i]]++;
+        suffix[i] = freq.size();
+       }
+       int ans= 0; 
+       for(int i=1; i<n; i++){
+        if(pref[i-1] == suffix[i]){
+            ans++;
+        }
+       }
+       return ans;
+    }
+};
