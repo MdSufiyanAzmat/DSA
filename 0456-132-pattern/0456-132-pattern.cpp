6b7ef357d44f681003b6e8649f59623875e5be4e
Md Sufiyan Azmat <71426452+MdSufiyanAzmat@users.noreply.github.com>
2026-05-30 11:51:12 +0530

    Time: 4 ms (95.32%), Space: 70.7 MB (70.36%) - LeetHub

---
+++ 0456-132-pattern/0456-132-pattern.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+    int n = nums.size();
+    int third = INT_MIN;
+    stack<int> s;
+    for(int i=n-1; i>=0; i--){
+        if(third > nums[i]){
+            return true;
+        }
+    while(!s.empty() && s.top() < nums[i]){
+        third = s.top();
+        s.pop();
+    }
+    s.push(nums[i]);
+    }
+    return false;
+    }
+};
