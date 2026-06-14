6e21ca2d6cc8e098fb979c32868611c40b96bce8
Md Sufiyan Azmat <71426452+MdSufiyanAzmat@users.noreply.github.com>
2026-06-14 20:42:47 +0530

    Time: 79 ms (5.13%), Space: 159.9 MB (5.01%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -10,19 +10,37 @@
  */
 class Solution {
 public:
+
+ListNode* reverse(ListNode* head){
+
+        ListNode* curr = head;
+        ListNode* newHead = NULL;
+      while(curr != NULL){
+        ListNode* newNode = new ListNode(curr-> val);
+        newNode-> next = newHead;
+        newHead = newNode;
+        curr = curr -> next;
+      }
+      return newHead;
+}
     int pairSum(ListNode* head) {
+       ListNode* dummy = reverse(head);
+       ListNode* temp = head;
+       int n = 0;
+       while(temp != NULL){
+        temp = temp -> next;
+        n++;
+       }
+       int a = 0;
        int ans = 0;
-       int maxAns = INT_MIN;
-       vector<int> v;
-       while(head != nullptr){
-        v.push_back(head->val);
+       int maxi = INT_MIN;
+       while(a < n / 2){
+        ans = head -> val + dummy -> val;
         head = head -> next;
+        dummy = dummy -> next;
+        maxi = max(ans, maxi);
+        a++;
        }
-       int n = v.size();
-       for(int i=0; i<(n/2); i++){
-        ans = v[i] + v[n-i-1];
-        maxAns = max(maxAns, ans);
-       }
-       return maxAns;
+       return maxi;
     }
 };
